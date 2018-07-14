27798274e9a15e8bcdfc26e3f2d584f571137665
NathanLacey <nathn.lacey@hotmail.com>
2018-07-14 15:58:11 -0700

    [Downloading] - Worked on downloading resume from site

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,10 @@ import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 
+import { FaCloudDownload } from "react-icons/lib/fa"
+
+const pdf = require('../assets/resources/Resume.pdf')
+
 class Main extends React.Component {
   render() {
 
@@ -33,6 +37,9 @@ class Main extends React.Component {
           <h2 className="major">About</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
           <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <a href={pdf} download="Resume_NathanLacey.pdf">
+              <FaCloudDownload/>Download Resume
+          </a>
           {close}
         </article>
 
@@ -41,15 +48,15 @@ class Main extends React.Component {
           <form method="post" action="#">
             <div className="field half first">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" id="name" required="true" placeholder="Jane Doe"/>
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="text" name="email" id="email" required="true" placeholder="example@website.ca"/>
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" id="message" rows="4" required="true"></textarea>
             </div>
             <ul className="actions">
               <li><input type="submit" value="Send Message" className="special" /></li>
