808cbbfcd3d6b4bf5daba637f7ab73982b232576
NathanLacey <nathn.lacey@hotmail.com>
2018-09-10 23:58:17 -0700

    c

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,6 @@ import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 import picLogo from '../images/logo.png'
 
-import { Document } from 'react-pdf'
 const pdf = require('../assets/resources/Resume.pdf')
 
 class Main extends React.Component {
